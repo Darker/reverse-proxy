1ef311c0ae4d02c67511c84b7d4c32962499a3a8
Jakub Mareda <mareda@techsys.cz>
2018-06-04 00:23:18 +0200

    Added console args for master server

--- app.js
+++ app.js
@@ -2,7 +2,22 @@
 const ProxyServer = require("./local/ProxyServer");
 const ProxyServerUDP = require("./local/ProxyServerUDP");
 
-const remote = new ManagementServer(process.env.PORT, 80);
+const socket_io_port = process.argv[1] * 1;
+if (socket_io_port * 1 != socket_io_port) {
+    throw new Error("Port for socket IO must be the first argument!");
+}
+else {
+    socket_io_port = socket_io_port * 1;
+}
+const tcp_proxy_port = process.argv[2] * 1;
+if (tcp_proxy_port * 1 != tcp_proxy_port) {
+    throw new Error("Port for TCP proxy server must be the second argument!");
+}
+else {
+    tcp_proxy_port = tcp_proxy_port * 1;
+}
+
+const remote = new ManagementServer(socket_io_port, tcp_proxy_port);
 
 
 //const local = new ProxyServer({
