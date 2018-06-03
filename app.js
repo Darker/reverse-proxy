2f11534c5e250e27cc2a9a76f48ef3c402235986
Jakub Mareda <mareda@techsys.cz>
2018-06-04 00:25:13 +0200

    Fixed argument indexing

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 const ProxyServer = require("./local/ProxyServer");
 const ProxyServerUDP = require("./local/ProxyServerUDP");
 
-const socket_io_port = process.argv[1] * 1;
+const socket_io_port = process.argv[2] * 1;
 if (socket_io_port * 1 != socket_io_port) {
     throw new Error("Port for socket IO must be the first argument!");
 }
 else {
     socket_io_port = socket_io_port * 1;
 }
-const tcp_proxy_port = process.argv[2] * 1;
+const tcp_proxy_port = process.argv[3] * 1;
 if (tcp_proxy_port * 1 != tcp_proxy_port) {
     throw new Error("Port for TCP proxy server must be the second argument!");
 }
